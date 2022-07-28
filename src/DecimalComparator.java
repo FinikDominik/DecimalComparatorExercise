b178c8647e1497fa376eac043758584d7ad7312c
Dominik Fínik <92373597+FinikDominik@users.noreply.github.com>
2022-07-28 10:10:57 +0200

    Update DecimalComparator.java

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class DecimalComparator {
+public class DecimalComparator {
+
+    public static boolean areEqualByThreeDecimalPlaces(double a, double b) {
+        float checkNumber1 = (float) (a * 100);
+        float checkNumber2 = (float) (b * 100);
+        if (checkNumber1 - checkNumber2 == 0) {
+            System.out.println("thrue, " + a + ", " + b);
+            return true;
+        } else
+            System.out.println("false, " + a + ", " + b);
+            return false;
+    }
+
 }
